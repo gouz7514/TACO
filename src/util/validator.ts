86f631245bc0b9d146e661e0c16856fd23606335
gouz7514 <gouz7514@gmail.com>
2024-01-03 18:35:07 +0900

    Add : validator를 별도의 파일로 분리

---
+++ src/util/validator.ts
@@ -0,0 +1,12 @@
+export const isEmail = (value: string) => {
+  const regExp = /^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[A-Za-z]+$/
+  return regExp.test(value)
+}
+
+export const isLongerThanSix = (value: string) => {
+  return value.length >= 6
+}
+
+export const isSamePassword = (password: string, passwordConfirm: string) => {
+  return password === passwordConfirm
+}
