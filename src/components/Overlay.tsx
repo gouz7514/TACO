4ddd73c70dc05a7e29c1e857a29134b9bae50946
gouz7514 <gouz7514@gmail.com>
2024-01-03 17:23:31 +0900

    Fix : Overlay component lint

--- src/components/Overlay.tsx
+++ src/components/Overlay.tsx
@@ -31,13 +31,13 @@ const Overlay = ({ onClickOverlay }: { onClickOverlay?: () => void }) => {
       position: fixed; 
       top: -${window.scrollY}px;
       overflow-y: scroll;
-      width: 100%;`;
+      width: 100%;`
     return () => {
-      const scrollY = document.body.style.top;
-      document.body.style.cssText = '';
-      window.scrollTo(0, parseInt(scrollY || '0', 10) * -1);
-    };
-  }, []);
+      const scrollY = document.body.style.top
+      document.body.style.cssText = ''
+      window.scrollTo(0, parseInt(scrollY || '0', 10) * -1)
+    }
+  }, [])
   
   return <OverlayStyle onClick={onClickOverlay} />
 }
