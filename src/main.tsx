96ed1a5d04483c33b2cc3fa6c92c07752fca5331
gouz7514 <gouz7514@gmail.com>
2024-01-04 15:00:17 +0900

    Update : AuthProvider 사용

--- src/main.tsx
+++ src/main.tsx
@@ -1,23 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
+import { AuthProvider } from './contexts/AuthContext.tsx'
 import GlobalStyle from './styles/GlobalStyle.ts'
 import MainLayout from './components/layouts/MainLayout.tsx'
 import Landing from './pages/Landing.tsx'
 import SignIn from './pages/SignIn.tsx'
 import SignUp from './pages/SignUp.tsx'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <GlobalStyle />
     <BrowserRouter>
-      <Routes>
-        <Route element={<MainLayout />}>
-          <Route path="/" element={<Landing />} />
-          <Route path="/login" element={<SignIn />} />
-          <Route path="/signup" element={<SignUp />} />
-        </Route>
-      </Routes>
+      <AuthProvider>
+        <Routes>
+          <Route element={<MainLayout />}>
+            <Route path="/" element={<Landing />} />
+            <Route path="/login" element={<SignIn />} />
+            <Route path="/signup" element={<SignUp />} />
+          </Route>
+        </Routes>
+      </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>
 )
