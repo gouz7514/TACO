51407fa1d49d5d81b1e308ae1c1dde1fbdb27289
gouz7514 <gouz7514@gmail.com>
2024-01-10 19:12:13 +0900

    Style : InputForm 자체에 스타일 적용

--- src/components/Organism/InputForm.tsx
+++ src/components/Organism/InputForm.tsx
@@ -6,20 +6,29 @@ type InputFormProps = {
   children: React.ReactNode
 }
 
-const InputFormStyle = styled.form`
-  width: 100%;
+const InputFormStyle = styled.div`
+  height: calc(100vh - 108px);
   display: flex;
-  flex-direction: column;
-  gap: 8px;
-  border: 1px solid #ccc;
-  padding: 24px;
-  border-radius: 8px;
+  justify-content: center;
+  align-items: center;
+  
+  form {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    gap: 8px;
+    border: 1px solid #ccc;
+    padding: 24px;
+    border-radius: 8px;
+  }
 `
 
 const InputForm = ({ className, onSubmit, children }: InputFormProps) => {
   return (
-    <InputFormStyle className={className} onSubmit={onSubmit}>
-      {children}
+    <InputFormStyle>
+      <form className={className} onSubmit={onSubmit}>
+        {children}
+      </form>
     </InputFormStyle>
   )
 }
